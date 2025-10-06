ed41142de4b639218c5e1231783a61d1738720f0
tigran420 <stigran420@gmail.com>
2025-10-06 21:21:50 +0400

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType # type: ignore
 import requests # type: ignore
 
 TELEGRAM_TOKEN = "8295931339:AAEP07XBDZ7FBIGSZg7SOZ8g7Sc_hsml8h0"
-TELEGRAM_CHAT_ID = "-1003166604153" # Замените на корректный ID вашей группы/канала Telegram. Для групп ID обычно начинается с -100.
 VK_TOKEN = "vk1.a.Do3IzROgiVPPGSjBVw3nFEg2eIAsy7673mBTpwakOxj_qNTtCxEXx8Pa9NS_q7FbDZqVlfecQgofYCYotRguILuXWAYu7DL2gkQocsu7zcRvk3M9R_0jCzzjErAJRLcy_Zx4jEZR87zCFUJvKIvkU_hLmJbfozuPkamZbBaElI1yZ8U3RpRNqMdjkdwm5SdFFS1HqCp7xxLu0EnF4JyVqA"
 VK_GROUP_ID = "233089872"
 # URL изображений
@@ -40,7 +39,7 @@ MATERIAL_PHOTOS = {
 
 # В функции send_telegram_application используйте TELEGRAM_TOKEN вместо TELEGRAM_BOT_TOKEN
 def send_telegram_application(application_data):
-    if not TELEGRAM_TOKEN or not TELEGRAM_CHAT_ID:
+    if not TELEGRAM_TOKEN:
         logging.warning("Telegram bot token or chat ID not configured. Skipping sending application to Telegram group.")
         return
 
@@ -50,7 +49,6 @@ def send_telegram_application(application_data):
 
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
     payload = {
-        "chat_id": TELEGRAM_CHAT_ID,
         "text": message_text,
         "parse_mode": "HTML"
     }
@@ -680,7 +678,7 @@ class KeyboardManager:
                         {
                             "action": {
                                 "type": "callback",
-                                "label": "💰 Эконом - - (до 150 тыс руб)",
+                                "label": "💰 Эконом - (до 150 тыс руб)",
                                 "payload": "{\"command\": \"бюджет_эконом\"}"
                             },
                             "color": "primary"
